a097151b07beb919b598de8e8bc6d39830e17604
berrylradianh <berrylhamaesha@gmail.com>
2023-11-21 00:16:53 +0700

    fix - debug dummy

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -11,17 +11,14 @@ class PagesController extends Controller
     //
     public function invoice_pending(){
         $datas = Invoice::all();
-        // dd($datas);
         return view('invoice_pending', compact('datas'));
     }
     public function upload_file_invoice($id){
         $data = Invoice::findorfail($id);
-        // dd($invoice);
         return view('upload_file_invoice', compact('data'));
     }
     public function table(){
         $datas = Invoice::all();
-        // dd($datas);
         return view('table', compact('datas'));
     }
 }
