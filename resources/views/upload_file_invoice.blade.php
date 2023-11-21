9b57da6bcf2aba3faa2ded87fa0eacb3a73d286c
berrylradianh <berrylhamaesha@gmail.com>
2023-11-21 10:55:41 +0700

    fix - css edit page

--- resources/views/upload_file_invoice.blade.php
+++ resources/views/upload_file_invoice.blade.php
@@ -12,13 +12,17 @@
 
     <!-- BEGIN GLOBAL MANDATORY STYLES -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
-    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <link href="assets/css/plugins.css" rel="stylesheet" type="text/css" />
+    <!-- <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" /> -->
+    <link href="{{asset('bootstrap/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{asset('assets/css/plugins.css')}}" rel="stylesheet" type="text/css" />
+    <!-- <link href="assets/css/plugins.css" rel="stylesheet" type="text/css" /> -->
     <!-- END GLOBAL MANDATORY STYLES -->
 
     <!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM STYLES -->
-    <link href="plugins/apex/apexcharts.css" rel="stylesheet" type="text/css">
-    <link href="assets/css/dashboard/dash_1.css" rel="stylesheet" type="text/css" />
+    <link href="{{asset('plugins/apex/apexcharts.css')}}" rel="stylesheet" type="text/css" />
+    <!-- <link href="plugins/apex/apexcharts.css" rel="stylesheet" type="text/css"> -->
+    <link href="{{asset('assets/css/dashboard/dash_1.css')}}" rel="stylesheet" type="text/css" />
+    <!-- <link href="assets/css/dashboard/dash_1.css" rel="stylesheet" type="text/css" /> -->
     <!-- END PAGE LEVEL PLUGINS/CUSTOM STYLES -->
 
 </head>
