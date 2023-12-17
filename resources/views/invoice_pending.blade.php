75dffcf91a604bdf07e9e9b289bcf26e36dcca27
berrylradianh <berrylhamaesha@gmail.com>
2023-12-18 02:39:01 +0700

    fix: navbar

--- resources/views/invoice_pending.blade.php
+++ resources/views/invoice_pending.blade.php
@@ -43,7 +43,7 @@
                     <line x1="3" y1="18" x2="21" y2="18"></line>
                 </svg></a>
 
-            <ul class="navbar-item flex-row ml-auto">
+                <ul class="navbar-item flex-row ml-auto">
                 <li class="nav-item dropdown user-profile-dropdown  order-lg-0 order-1">
                     <a href="javascript:void(0);" class="nav-link dropdown-toggle user" id="userProfileDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user-check">
@@ -59,6 +59,14 @@
                                     <h5>{{Auth::user()->email}}</h5>
                                 </div>
                             </div>
+                        </div>
+                        <div class="dropdown-item">
+                            <a href="{{url('profile')}}">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user">
+                                    <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+                                    <circle cx="12" cy="7" r="4"></circle>
+                                </svg> <span>My Profile</span>
+                            </a>
                         </div>
                         <div class="dropdown-item">
                             <a href="{{route('logout')}}">
